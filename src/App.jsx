e7255d48bed75567f851d6d5889d25f4ba96ef2a
danielcanunciato <danielcanunciatooliveira@gmail.com>
2026-07-11 01:04:36 -0300

    TCC BG

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ export default function App() {
                   "flexDirection" : "column",
                   "alignItems" : "center",
                   "width" : "75%",
-                  "--bg-image": `url(/represent/backend-wp.jpg)`,
+                  "--bg-image": `url(/represent/tcc-wp.png)`,
               }}
           />
       </div>
